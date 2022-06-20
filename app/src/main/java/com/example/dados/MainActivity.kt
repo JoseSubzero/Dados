06b182683b2e4b8c0e1ef101add8a15d275ae769
Alumno <gsdag>
2022-06-20 19:08:51 +0200

    Commit final. Boton quitado y funcion mas optimizada quitando codigo irrelevante

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -8,33 +8,28 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val dados = arrayOf(R.drawable.dice1, //Son enteros basados en su indentificaor como si fueran un codigo de barras/etiqueta
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
     }
 
     fun random(view: View) {
-        val n1 = Random.nextInt(1, 7)
-        val n2 = Random.nextInt(1, 7)
-        binding.ivDado1.setImageResource(R.drawable.dice1)
-        binding.ivDado2.setImageResource(R.drawable.dice2)
-        val imgResource = when (n1) {
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
-        }
-        val imgResource2 = when (n2) {
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
+        val d1 = Random.nextInt(1, 7)
+        val d2 = Random.nextInt(1, 7)
+        binding.ivDado1.setImageResource(dados[d1 -1])
+        binding.ivDado2.setImageResource(dados [d2-1])
+        binding.tvResultado.text = (d1 + d2).toString()
 
-        }
+
+
     }
 }
